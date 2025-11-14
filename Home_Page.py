5fa5730a3b1aefaa904887d3d79d4e15ed58fdf5
krisbhavsar <krisbhavsar123@gmail.com>
2025-11-14 17:49:09 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -21,10 +21,9 @@ st.subheader("Jimmy L, Kris B")
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1. **Golden Boot Tracker **: This tool provides information on the selected amount of top goal scorers in the English Premier League. Users can further select player's they wish to view more detailed stats of.
-2.
+1. **Golden Boot Tracker**: This tool provides information on the selected amount of top goal scorers in the English Premier League. Users can further select player's they wish to view more detailed stats of.
+2. **
 3.
-4.
 
 """)
-
+st.image(webdev3/Images/soccer.jpg)
