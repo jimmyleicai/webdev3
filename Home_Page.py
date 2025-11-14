3031427c620849dce8e41ce24444979b09ff1102
krisbhavsar <krisbhavsar123@gmail.com>
2025-11-14 17:46:27 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -13,7 +13,7 @@ st.subheader("Jimmy L, Kris B")
 
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
-        1. **Golden Boot Tracker **: This tool provides information on the selected amount of top goal scorers in the English Premier League. Users can further select player's they wish to view more detailed stats of.
+#       1. **Page Name**: Description
 #       2. **Page Name**: Description
 #       3. **Page Name**: Description
 #       4. **Page Name**: Description
@@ -21,7 +21,7 @@ st.subheader("Jimmy L, Kris B")
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1.
+1. **Golden Boot Tracker **: This tool provides information on the selected amount of top goal scorers in the English Premier League. Users can further select player's they wish to view more detailed stats of.
 2.
 3.
 4.
