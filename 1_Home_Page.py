ff75ddffdc639606ea77c9ea9bd84a607707c159
jimmyleicai <jimmyleicai@gmail.com>
2025-11-30 20:48:40 -0400

    Add files via upload

---
+++ 1_Home_Page.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 68, Web Development - Section C")
+st.subheader("Jimmy L, Kris B")
+
+st.subheader("")
+st.image("Images/messi.jpg")
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Golden Boot**: dnsjdfnsdfns
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. ⭐Golden Boot Contenders: A dynamic tool that displays data on the top Premier League scorers in the current season.
+2. ⚽Premier League Matchup Analyzer: Get predictions on hypothetical matches between two Premier League teams based on the latest data.
+3. 🌐EPL Chatbot: Chat with Gemini about the Premier League's top scorers based on the latest data.
+
+""")
+
